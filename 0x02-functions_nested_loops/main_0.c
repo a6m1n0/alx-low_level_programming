da4eb2d385273df84ad6a1b88500b06aa56a1643
user3140 <manger.jsm@gmail.com>
2023-08-15 20:01:14 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -7,6 +7,19 @@ int main(void)
 
 r = _abs(-1);
 r = print_sign(98);
-
+r = _islower('H');
+		
+    _putchar(r + '0');
+		
+    r = _islower('o');
+		
+    _putchar(r + '0');
+		
+    r = _islower(108);
+		
+    _putchar(r + '0');
+		
+    _putchar('\n');
+r = print_last_digit(98);
     return 0;
 }
