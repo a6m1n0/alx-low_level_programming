b85749e33df6def9a96f55ed4dd28703a68119ff
user3140 <manger.jsm@gmail.com>
2023-08-15 19:52:48 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
-
-	/**
-	 * _abs - computes the absolute value of an integer
-	 * @n: the int to check
-	 * Return: the absolute value of int
-	 */
+/**
+ * print_sign - prints the sign of a number
+ * @n: the number to check
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
 	int _abs(int n)
 	{
 		if (n >= 0)
