ac1f1986e4aebb6bb9c5e13c8a16b143a57dd516
user3140 <manger.jsm@gmail.com>
2023-08-15 19:35:21 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,3 +1,5 @@
+#include <ctype.h>  // Include the appropriate header for _isalpha
+
 #include "main.h"
 
 /**
@@ -7,5 +9,5 @@
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
