568bc85ac5719ce23cd9bde3bb9eb70784deae6d
user3140 <manger.jsm@gmail.com>
2023-08-15 19:45:32 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/main_0.c
+++ 0x02-functions_nested_loops/main_0.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-    int n = 'c';  // Replace 'c' with the character you want to check
+    int n = 'a';  // Replace 'c' with the character you want to check
     int r;
 
-    r = _islower(n);
-    r = _isalpha(n);
+
+r = _isalpha(n);
 
     return 0;
 }
