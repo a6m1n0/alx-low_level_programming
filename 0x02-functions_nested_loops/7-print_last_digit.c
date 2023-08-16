bc4ef15127ec951ded6df9151c4784fd5f8d799b
user3140 <manger.jsm@gmail.com>
2023-08-15 19:58:13 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,11 +4,20 @@
  * @n: the integer to compute the absolute value for
  * Return: the absolute value of n
  */
-	int _abs(int n)
-	{
-		if (n >= 0)
-		{
-			return (n);
-		}
-		return (-n);
-	}
+int print_last_digit(int n)
+{
+	int a;	
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+		
+	if (a < 0)
+		a = -a;
+	
+	_putchar(a + '0');
+	
+	return (a);
+		
+}
