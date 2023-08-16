c154c4f8d11469f2dec87504ac17676511f00f80
user3140 <manger.jsm@gmail.com>
2023-08-15 19:31:04 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,11 @@
+#include "main.h"  // Include the header file with the function declaration
+
+int main(void)
+{
+    int n = 'c';  // Replace 'c' with the character you want to check
+    int r;
+
+    r = _islower(n);  // Call the correct function _islower
+
+    return 0;
+}
