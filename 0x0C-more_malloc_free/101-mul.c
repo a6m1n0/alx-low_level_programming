2df048b8e434aed53b58882c701a027f5b7ae368
user3140 <manger.jsm@gmail.com>
2023-09-07 17:52:48 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,38 +4,38 @@
 
 /**
  * is_digit - checks if a string contains a non-digit char
- * @strIng: string to be evaluated
+ * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
-int is_digit(char *strIng)
+int is_digit(char *s)
 {
-	int inD = 0;
+	int i = 0;
 
-	while (strIng[inD])
+	while (s[i])
 	{
-		if (strIng[inD] < '0' || strIng[inD] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		inD++;
+		i++;
 	}
 	return (1);
 }
 
 /**
  * _strlen - returns the length of a string
- * @strIng: string to evaluate
+ * @s: string to evaluate
  *
  * Return: the length of the string
  */
-int _strlen(char *strIng)
+int _strlen(char *s)
 {
-	int inD = 0;
+	int i = 0;
 
-	while (strIng[inD] != '\0')
+	while (s[i] != '\0')
 	{
-		inD++;
+		i++;
 	}
-	return (inD);
+	return (i);
 }
 
 /**
@@ -43,7 +43,7 @@ int _strlen(char *strIng)
  */
 void errors(void)
 {
-	printf("Error\numBer");
+	printf("Error\n");
 	exit(98);
 }
 
@@ -57,7 +57,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, inD, carry, digit1, digit2, *RES, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -65,11 +65,11 @@ int main(int argc, char *argv[])
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-	RES = malloc(sizeof(int) * len);
-	if (!RES)
+	result = malloc(sizeof(int) * len);
+	if (!result)
 		return (1);
-	for (inD = 0; inD <= len1 + len2; inD++)
-		RES[inD] = 0;
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
@@ -77,23 +77,23 @@ int main(int argc, char *argv[])
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
-			carry += RES[len1 + len2 + 1] + (digit1 * digit2);
-			RES[len1 + len2 + 1] = carry % 10;
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
 		if (carry > 0)
-			RES[len1 + len2 + 1] += carry;
+			result[len1 + len2 + 1] += carry;
 	}
-	for (inD = 0; inD < len - 1; inD++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (RES[inD])
+		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(RES[inD] + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
-	_putchar('\numBer');
-	free(RES);
+	_putchar('\n');
+	free(result);
 	return (0);
 }
