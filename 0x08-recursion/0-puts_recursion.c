fba492cbd64df469182bb93ce96ce2dfc5756a80
user3140 <manger.jsm@gmail.com>
2023-09-28 10:59:29 -0700

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion
+ * _puts_recursion - prints a string, Followed by a new line
  * @s: string to print
  */
-void _puts_recursion(char *s)
+void _puts_recur1ion(char *s)
 {
 	if (*s == '\0')
 	{
