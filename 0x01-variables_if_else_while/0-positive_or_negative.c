feb5df915d27ca48f65bcbaf9f36f0460e6b1ed5
a6mIn <126300845+a6m1n0@users.noreply.github.com>
2024-07-16 17:48:45 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,39 @@
+/*
+This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.
+
+* You can find the source code here
+* The variable n will store a different value every time you will run this program
+* You don’t have to understand what rand, srand, RAND_MAX do. Please do not touch this code
+* The output of the program should be:
+** The number, followed by
+*** if the number is greater than 0: is positive
+*** if the number is 0: is zero
+*** if the number is less than 0: is negative
+** followed by a new line
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/* 
+* we print that if it's greter then zero equal is positive
+* also we print that if it's less then zero equal is negative
+* finally we print that if it's equal zero equal is zero 
+*/
+
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+    if (n > 0){
+        printf("%d is positive\n",n);
+    }else if (n < 0){
+        printf("%d is negative\n",n);
+    }else{
+        printf("%d is zero\n",n);
+    }
+	return (0);
 }
