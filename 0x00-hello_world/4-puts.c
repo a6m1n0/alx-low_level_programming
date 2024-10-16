d637c7b2c46eaa306c71a4cfd17866b78c56340e
a6mIn <126300845+a6m1n0@users.noreply.github.com>
2024-10-16 16:24:32 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,41 @@
+/*
+Write a C program that prints exactly 
+`"Programming is like building a multilingual puzzle`
+followed by a new line.
+
+Use the function puts
+You are not allowed to use printf
+Your program should end with the value 0
+*/
+
+
 #include <stdio.h>
-/**
+
+int main(){
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include <stdio.h>
+
 *main - Entry point
 *Description: A C program that prints with the puts function
 *Return: Always 0 (success)
-*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
+*/
