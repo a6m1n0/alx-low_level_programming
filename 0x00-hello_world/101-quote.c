b7bac92505e84582fa9f220c3079b5a8b24eca42
a6mIn <126300845+a6m1n0@users.noreply.github.com>
2024-10-16 16:55:21 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,40 @@
+/*
+Write a C program that prints exactly
+`and that piece of art is useful" - Dora Korpar, 2015-10-19`
+followed by a new line, to the standard error.
+
+
+You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
+Your program should return 1
+Your program should compile without any warnings when using the -Wall gcc option
+
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(void){
+    char *i = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    write(2,i, strlen(i));
+    return 1;
+}
+
+
+/**
 #include <stdio.h>
 #include <unistd.h>
-/**
+
 *main - Entry point
 *Description: A C program that prints with the puts function
 *Return: Always 0 (success)
-*/
+
 int main(void)
 {
 	write(2,
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
+*/
