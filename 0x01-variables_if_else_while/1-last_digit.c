e4c3da830d330a3f2b90a4ca75ecfb1735e5d240
a6mIn <126300845+a6m1n0@users.noreply.github.com>
2024-10-19 00:59:36 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,54 @@
+/*
+This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.
+
+You can find the source code here
+The variable n will store a different value every time you run this program
+You don’t have to understand what rand, srand, and RAND_MAX do. Please do not touch this code
+The output of the program should be:
+The string Last digit of, followed by
+n, followed by
+the string is, followed by
+if the last digit of n is greater than 5: the string and is greater than 5
+if the last digit of n is 0: the string and is 0
+if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+followed by a new line
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int LastDigit= n%10;
+
+    if (LastDigit > 5 )
+        printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
+
+    else if (LastDigit < 6 && LastDigit !=0)
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, LastDigit);
+    else 
+        printf("Last digit of %d is %d and is 0\n", n, LastDigit);    
+	return (0);
+}
+
+
+/**
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
+
  * main - Prints the last digit of a randomly generated number
  *        and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
- */
+  
 int main(void)
 {
 int n;
@@ -29,3 +71,4 @@ n, n % 10);
 }
 return (0);
 }
+ */
