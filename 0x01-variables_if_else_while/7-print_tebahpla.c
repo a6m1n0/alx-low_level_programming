f5152889be4e06f097b33c18d0e8d92ee6a43de7
a6mIn <126300845+a6m1n0@users.noreply.github.com>
2024-10-21 22:14:15 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,30 @@
+/*
+Write a program that prints the lowercase alphabet in reverse, followed by a new line.
+
+You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+All your code should be in the main function
+You can only use putchar twice in your code
+*/
+#include <stdio.h>
+
+int main (){
+
+    for (int alpha = 'z'; alpha >= 'a'; alpha--)
+        putchar(alpha);
+    putchar('\n');    
+    return 0;
+}
+
+
+/**
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
+
  * main - prints the lowercase alphabet in reverse,
  * followed by a new line
  * Return: Always 0 (Success)
- */
+
 int main(void)
 {
 char ch;
@@ -16,4 +35,4 @@ putchar(ch);
 putchar('\n');
 return (0);
 }
-
+ */
