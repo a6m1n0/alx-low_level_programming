fbf6d4883ea7cde881a7ca991722b8871b23c297
a6mIn <126300845+a6m1n0@users.noreply.github.com>
2024-10-21 21:51:28 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,32 @@
+/*
+Write a program that prints the alphabet in lowercase, followed by a new line.
+
+You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+All your code should be in the main function
+You can only use putchar twice in your code
+*/
+#include <stdio.h>
+
+int main (){
+ 
+    char alpha = 'a';
+
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
+        putchar(alpha);
+    putchar('\n');    
+    return 0;
+}
+
+/**
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-/**
+
  * main - prints the alphabet in lowercase,
  * followed by a new line
  * Return: Always 0 (Success)
- */
+
 int main(void)
 {
 char ch;
@@ -17,4 +37,4 @@ putchar(ch);
 putchar('\n');
 return (0);
 }
-
+ */
