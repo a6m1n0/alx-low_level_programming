ef4801b1000714952b331927102f3796fd1d1df8
a6mIn <126300845+a6m1n0@users.noreply.github.com>
2024-10-21 22:20:31 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,23 @@
+/*
+Write a program that prints the lowercase alphabet in reverse, followed by a new line.
+
+You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+All your code should be in the main function
+You can only use putchar twice in your code
+*/
+#include <stdio.h>
+
+int main (){
+
+    for (int digits = 0x30; digits <= 0x39; digits++)
+        putchar(digits);
+    for (int letters = 0x61; letters <= 0x66; letters++ )   
+        putchar(letters); 
+    putchar('\n');    
+    return 0;
+}
+
+/**
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -5,7 +25,7 @@
  * main - prints all the numbers of base 16 in lowercase,
  * followed by a new line
  * Return: Always 0 (Success)
- */
+ 
 int main(void)
 {
 int n;
@@ -22,3 +42,4 @@ putchar('\n');
 return (0);
 }
 
+ */
