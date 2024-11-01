d9b3dd4fc5a4439d29fbcb583d5b902f1d52bc1b
a6mIn <126300845+a6m1n0@users.noreply.github.com>
2024-11-01 07:39:14 +0200

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/*
+Write a program that prints _putchar, followed by a new line.
+
+The program should return 0
+*/
+
+#include <stdio.h>
+
+void printx(){
+    printf("_putchar\n");
+}
+
+int main() {
+
+    printx();
+    return 0;
+}
